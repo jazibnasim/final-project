019abeae5ea5a9dd0ef913c3d9a8729cbbc35089
Jazib Nasim <jazib.nasim@gmail.com>
2016-12-05 13:52:27 -0500

    script file comments

--- public/scripts.js
+++ public/scripts.js
@@ -1,19 +1,29 @@
-(function() {
-var app = angular.module("currentsee", ["ngRoute"]); // create the module
 
+
+(function() {
+
+//Module is setup here with ngRoute dependency injection
+var app = angular.module("currentsee", ["ngRoute"]); 
+
+
+// Creates the route
 app.config(function($routeProvider) {
+
+// Link to  Form View
     $routeProvider.when("/form", {
         templateUrl: "views/form.html",
         controller: 'formController'
 
     });
+
+// Link to  Result View
     $routeProvider.when("/result", {
         templateUrl: "views/result.html",
         controller: 'resultController'
+});
 
 
-
-    });
+//Main Page View
 
 
     $routeProvider.otherwise({
