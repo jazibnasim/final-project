fa12daab1b7bce3bda666e3375274936be6b15c2
nealcsimms <nealcsimms@gmail.com>
2016-12-12 14:14:46 -0500

    cleaned out the broken twitter gets from app.js. added in the eexpress server

--- neals_server.js
+++ neals_server.js
@@ -3,20 +3,25 @@ var express = require('express');
 var app = express();
 twit = require('twitter'),
 	twitter = new twit({
-		consumer_key: '',
-		consumer_secret: '',
-		access_token_key: '',
-		access_token_secret: ''
+		consumer_key: 'e4dPlJrNlpJFCjPkfB6YLegz3',
+		consumer_secret: '0ChN1PSoJA8cEcZJP1RLoACD0HnGHo45ToK0PODCECKfRITPlb',
+		access_token_key: '806166248212103169-K0dwcQu4FGg55cVIVTXo6wamB2yqs7O',
+		access_token_secret: '8qkRhWUcXFlp03FdB3UP3I6BYJIcny3YTQ5Y8IDTRxn4u'
 	});
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/tweets/:name/:city', function(req, res){
     var searchquery = req.params.name + ' ' + req.params.city;
+    console.log("working");
     twitter.get('search/tweets', {q: searchquery, count: 6}, function(error, tweets, response) {
-        //if no errors, send our tweets to our
+        console.log("it works");        //if no errors, send our tweets to our
         if (!error) {
             res.send(tweets);
+        }
+
+        else{
+            res.send(error);
         }
     });
 });
--- public/js/app.js
+++ public/js/app.js
@@ -77,7 +77,7 @@ app.controller('resultController', function($scope, currentseeFactory, $http){
                $scope.photos = [];
 
                 console.log("success", response);
-                for(var i = 0; i <= 5; i++) {
+                for(var i = 0; i <= 6; i++) {
                   $scope.photos.push($scope.photosrc = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=350&photoreference="+response.data.results[i].photos[0].photo_reference+"&key=AIzaSyCTKPL7rYEGDsf6NT_AFO8991Gb9QY3C-Y");
                 }
                 // console.log(testArray);
@@ -91,42 +91,5 @@ app.controller('resultController', function($scope, currentseeFactory, $http){
               $scope.photos = []
        });
 
-       $http({
-               method: 'GET',
-               url:'/tweets.json',
-               }).then(function successCallback(response){
 
-
-
-                   console.log("success", response);
-                   for(var i = 0; i <= 5; i++) {
-                     if (tweets.user[i]=== name) {
-                       $scope.tweets.push(tweets[i]);
-                     }
-
-                   }
-                   // console.log(testArray);
-                   // var photoRef = response.data.results[0].photos[0].photo_reference;
-
-
-
-                }, function errorCallback(response){
-                   console.log("Error", response);
-
-                 $scope.tweets = []
-          });
-
-
-
-
-
-
-
-});
-
-var resultTweet = '';
-
-$.get('/api/tweets', function(data){
-  resultTweet = data;
-$("#tweet").html(resultTweet);
 });
