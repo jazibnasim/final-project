17f536c7601186f193231d72d4342f6130e2fa94
Katie Kerkman <kkerkman@yahoo.com>
2016-12-13 11:20:31 -0500

    updated wording in app.get twitter

--- neals_server.js
+++ neals_server.js
@@ -11,8 +11,8 @@ twit = require('twitter'),
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/tweets/:name/:city', function(req, res){
-    var searchquery = req.params.name + ' ' + req.params.city;
+app.get('/tweets', function(req, res){
+    var searchquery = req.query.name + ' ' + req.query.city;
     console.log("working");
     twitter.get('search/tweets', {q: searchquery, count: 6}, function(error, tweets, response) {
         console.log("it works");        //if no errors, send our tweets to our
